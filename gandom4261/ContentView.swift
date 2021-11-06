45871f49ee627e97e83cdc2b7ce327192cfbff11
Hao <dinhhao285@gmail.com>
2021-11-06 09:49:46 -0400

    Initial Commit

---
+++ gandom4261/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gandom4261
+//
+//  Created by Hao Nguyen on 11/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gandom4261/gandom4261App.swift
@@ -0,0 +1,17 @@
+//
+//  gandom4261App.swift
+//  gandom4261
+//
+//  Created by Hao Nguyen on 11/6/21.
+//
+
+import SwiftUI
+
+@main
+struct gandom4261App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
